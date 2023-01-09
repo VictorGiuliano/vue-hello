126c884c2312ff4cefdb1f264156b282b1a47bf9
VictorGiuliano <victorgiuliano98@gmail.com>
2023-01-09 17:07:54 +0100

    stampa in pagina del messaggio

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.//
+
+const app = Vue.createApp({
+    data(){
+        return{message: 'Buongiornooooo Vietnam'}
+    }
+});
+
+app.mount('#root');
